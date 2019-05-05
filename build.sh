885f3ffe2aea12065ee8bebb6cc10836a4e37129
Syrus <me@syrusakbary.com>
2019-05-05 14:03:24 -0700

    Improved build script to output wasm

--- build.sh
+++ build.sh
@@ -22,18 +22,18 @@ mkdir -p $DIST
 # emscripten folks. They are using libpng-16 so its emperative to use this version
 # when linking the header files to avoid disparities later
 
-apt-get install -y libpng16-dev
+# apt-get install -y libpng16-dev
 
 # compile zlib
 cd $DIR/deps/pngquant/zlib
-emconfigure ./configure  --64
-emmake make
+# AR=llvm-ar ARFLAGS=rc emconfigure ./configure  --64
+# EXEEXT=.js LDFLAGS="-s WASM=1" emmake make
 
 # change dir to deps
 cd $DIR/deps/pngquant
 
 # start configuring script
-emconfigure ./configure --disable-sse --with-libpng=/usr/include
+AR=llvm-ar ARFLAGS=rc emconfigure ./configure --disable-sse --with-libpng=/usr/include --without-cocoa
 
 # At this point just make sure that config.mk file points correct version of
 # libpng
@@ -47,9 +47,12 @@ emmake make
 mv pngquant pngquant.bc
 
 # Time to rumble. Create our mighty js file
-emcc -03 pngquant.bc  -s TOTAL_MEMORY=335544323  -s USE_LIBPNG=1 -s USE_ZLIB=1  --pre-js ../../pre.js --post-js ../../post.js   -o pngquant.js
+emcc -03 pngquant.bc -s USE_LIBPNG=1 -s USE_ZLIB=1  --pre-js ../../pre.js --post-js ../../post.js -s WASM=1  -o pngquant.js
 
 # copy our js file to dist folder
 mv pngquant.js $DIST/pngquant.js
+mv pngquant.wasm $DIST/pngquant.wasm
+
+cp $DIST/pngquant.wasm $DIR/pngquant.wasm
 
 echo "pngquant.js has been successfully compiled  and placed in $DIST"
